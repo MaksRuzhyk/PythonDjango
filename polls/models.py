5b7975b51147b6dc2bf80cbac4428dd4ff8c1ab6
MaksRuzhyk <mruzilenki@gmail.com>
2024-10-05 18:38:27 +0300

    orm

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField()
+    create_data = models.DateTimeField()
+    update_data = models.DateTimeField()
+    content = models.TextField()
+    numbers_of_likes = models.IntegerField(default=0)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        pass
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    age = models.IntegerField()
+    number_of_posts = models.IntegerField(default=0)
+    class Meta:
+        pass
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,50 @@
+from datetime import datetime
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post, Author
+from .forms import PostForm
+from django.urls import reverse, reverse_lazy
 def index(request):
-    return HttpResponse('<h7>hello world</h7>')
+    context = {'massage':'hello'}
+    return render(request, 'index.html',context)
+
+def home_page(request):
+    posts = Post.objects.all()
+    authors = Author.objects.all()
+    context = {'posts':posts, 'author':authors}
+    return render(request, 'home_page.html',context)
+
+def add_post(request):
+
+    form = PostForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            saved = form.save(commit=False)
+            saved.create_data = datetime.now()
+            saved.update_data = datetime.now()
+
+            saved.save()
+            return HttpResponseRedirect(reverse_lazy('add_post'))
+
+    return render(request, 'add_post.html', {'form':form})
+
+
+def post_detail(request,pk):
+    post = Post.objects.get(pk=pk)
+    context = {'post':post}
+    return render(request,'post_detail.html',context)
+
+
+def author_page(request, pk):
+    author = Author.objects.get(pk=pk)
+    authors_posts = Post.objects.filter(author=author)
+    context = {'author':author,'authors_posts':authors_posts}
+    return render(request, 'author_page.html',context)
+
+def add_like(request,pk):
+    post = Post.objects.get(pk=pk)
+    post.numbers_of_likes +=1
+    post.save()
+    return render(request, 'post_detail.html', {'post': post})
+
